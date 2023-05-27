c4bc36d8b6c44dae8fab7b29deddf5b10e5b11dd
Kasymbek Tashbaev <kasymbek.rustambekovich@gmail.com>
2023-05-27 23:45:16 +0600

    update description

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 # flutter_sound_processing
 
-Flutter package designed for sound processing and analysis.
+Flutter library that provides audio processing capabilities, including the calculation of feature matrices from audio signals. It enables Flutter developers to extract meaningful information from audio data, such as Mel-Frequency Cepstral Coefficients (MFCCs) and spectral features.
 
-## Getting Started
+**Note: iOS support is currently in progress.**
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
-
-For help getting started with Flutter development, view the
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
 
--- lib/flutter_sound_processing.dart
+++ lib/flutter_sound_processing.dart
@@ -3,6 +3,22 @@ import 'dart:typed_data';
 import 'flutter_sound_processing_platform_interface.dart';
 
 class FlutterSoundProcessing {
+  /// Retrieves the feature matrix from the audio signals.
+  ///
+  /// [signals] is a list of double values representing the audio signals.
+  ///
+  /// [sampleRate] specifies the sample rate of the audio signals.
+  ///
+  /// [hopLength] determines the hop length (in samples) used for analysis.
+  ///
+  /// [nMels] sets the number of Mel filters to be used in the analysis.
+  ///
+  /// [fftSize] defines the size of the FFT used for spectral analysis.
+  ///
+  /// [mfcc] specifies the number of MFCC coefficients to be calculated.
+  ///
+  /// The method returns a `Future<Float64List>` that resolves to the feature matrix,
+  /// or null if there was an error during the calculation.
   Future<Float64List?> getFeatureMatrix({
     required List<double> signals,
     required int sampleRate,
